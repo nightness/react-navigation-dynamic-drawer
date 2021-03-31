4b208f3682019ec673a0f70aa88266dea0bc3e03
nightness <joshguyette@yahoo.com>
2021-03-30 22:46:19 -0500

    snapshot

--- screens/Dynamic.tsx
+++ screens/Dynamic.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DrawerNavigationProp } from '@react-navigation/drawer'
 import { Text } from 'react-native'
 import Screen from '../components/Screen'
-import NavigationControls from '../components/NavigationControls'
+import NavigationControls from '../components/TestPanel'
 
 interface Props {
     navigation: DrawerNavigationProp<any>
--- screens/Playground.tsx
+++ screens/Playground.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Text } from 'react-native'
 import { DrawerNavigationProp } from '@react-navigation/drawer'
 import Screen from '../components/Screen'
-import NavigationControls from '../components/NavigationControls'
+import NavigationControls from '../components/TestPanel'
 
 interface Props {
     navigation: DrawerNavigationProp<any>
