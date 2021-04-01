653e37a6cb139077ab42e35950e5b806f9bbed2e
nightness <joshguyette@yahoo.com>
2021-03-31 23:52:12 -0500

    refactoring

--- navigation/RoutingReducer.tsx
+++ navigation/RoutingReducer.tsx
@@ -8,7 +8,6 @@ export type ScreenAction = {
     screen?: NavigationElement,
     index?: number,
     name?: string,
-    children?: NavigationElements
 }
 
 export const ScreensReducer = (currentState: NavigationElements, action: ScreenAction) => {
